0c01723672239b5bc7554c279a8e346d3528f72b
dragon-liu <liuzhichao@hust.edu.cn>
2021-09-10 23:30:57 +0800

    12.5.5

--- chapter12/scripts/global.js
+++ chapter12/scripts/global.js
@@ -53,6 +53,8 @@ function highlightPage(){
   }
 }
 
+// Home
+
 function moveElement(elementID,final_x,final_y,interval) {
   if (!document.getElementById) return false;
   if (!document.getElementById(elementID)) return false;
@@ -139,6 +141,8 @@ function prepareSlideshow() {
     }
   }
 
+// About
+
 function showSection(id){
   var sections = document.getElementsByTagName("section");
   for(var i = 0; i < sections.length; i++){
@@ -170,6 +174,8 @@ function prepareInternalnav(){
     }
   }
 }
+
+// Photos
 
 //展示相应图片和文字描述
 function showPic(whichpic){
@@ -223,10 +229,80 @@ function prepareGallery(){
   }
 }
 
+// Live
+
+function stripeTables() {
+  if (!document.getElementsByTagName) return false;
+  var tables = document.getElementsByTagName("table");
+  for (var i=0; i<tables.length; i++) {
+    var odd = false;
+    var rows = tables[i].getElementsByTagName("tr");
+    for (var j=0; j<rows.length; j++) {
+      if (odd == true) {
+        addClass(rows[j],"odd");
+        odd = false;
+      } else {
+        odd = true;
+      }
+    }
+  }
+}
+
+function highlightRows() {
+  if(!document.getElementsByTagName) return false;
+  var rows = document.getElementsByTagName("tr");
+  for (var i=0; i<rows.length; i++) {
+    rows[i].oldClassName = rows[i].className
+    rows[i].onmouseover = function() {
+      addClass(this,"highlight");
+    }
+    rows[i].onmouseout = function() {
+      this.className = this.oldClassName
+    }
+  }
+}
+
+function displayAbbreviations() {
+  if (!document.getElementsByTagName || !document.createElement || !document.createTextNode) return false;
+  var abbreviations = document.getElementsByTagName("abbr");
+  if (abbreviations.length < 1) return false;
+  var defs = new Array();
+  for (var i=0; i<abbreviations.length; i++) {
+    var current_abbr = abbreviations[i];
+    if (current_abbr.childNodes.length < 1) continue;
+    var definition = current_abbr.getAttribute("title");
+    var key = current_abbr.lastChild.nodeValue;
+    defs[key] = definition;
+  }
+  var dlist = document.createElement("dl");
+  for (key in defs) {
+    var definition = defs[key];
+    var dtitle = document.createElement("dt");
+    var dtitle_text = document.createTextNode(key);
+    dtitle.appendChild(dtitle_text);
+    var ddesc = document.createElement("dd");
+    var ddesc_text = document.createTextNode(definition);
+    ddesc.appendChild(ddesc_text);
+    dlist.appendChild(dtitle);
+    dlist.appendChild(ddesc);
+  }
+  if (dlist.childNodes.length < 1) return false;
+  var header = document.createElement("h3");
+  var header_text = document.createTextNode("Abbreviations");
+  header.appendChild(header_text);
+  var articles = document.getElementsByTagName("article");
+  if (articles.length == 0) return false;
+  articles[0].appendChild(header);
+  articles[0].appendChild(dlist);
+}
+
 // Load events
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
 addLoadEvent(prepareGallery);
 addLoadEvent(preparePlaceholder);
+addLoadEvent(stripeTables);
+addLoadEvent(highlightRows);
+addLoadEvent(displayAbbreviations);
 
